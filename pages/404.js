5ea8af103e499cedb1df5f5b6920f60062d9d502
KillerDogs <kenybustamante2@hotmail.com>
2023-09-21 15:53:02 -0300

    Edit custom404

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Breadcrumbs from "@/components/Breadcrumbs";
+import React from "react";
+
+const Custom404 = () => {
+    return(
+        <div>
+            <Breadcrumbs/>
+            <h1>Ops, algo deu errado! Esta página não existe!</h1>
+        </div>
+    )
+}
+
+export default Custom404;
