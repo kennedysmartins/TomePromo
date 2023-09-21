41691016e5e62ed8b20f761442ea27d4dcf2eebb
KillerDogs <kenybustamante2@hotmail.com>
2023-09-21 20:15:45 -0300

    Item ID model

--- pages/promo/[ItemId].js
+++ pages/promo/[ItemId].js
@@ -1,12 +1,18 @@
 import React from "react";
-import axios from "axios";
-import { Container } from "@/components/Container";
+import { Container } from "@/components/Container"
+import { Content } from "@/components/Content"
+import { Header } from "@/components/Header"
+import 'tailwindcss/tailwind.css';
+import { Card } from "@/components/Card";
 
 
 export function promoPage() {
     return (
         <Container>
-
+          <Header />
+          <Content>
+            <Card/>
+        </Content>
         </Container>
     )
 }
