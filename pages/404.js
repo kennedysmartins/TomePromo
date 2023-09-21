c85ebfd3e6236a5084451257f3be900db0cade1a
KillerDogs <kenybustamante2@hotmail.com>
2023-09-21 20:15:16 -0300

    404 changes

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,29 @@
-import Breadcrumbs from "@/components/Breadcrumbs";
 import React from "react";
+import { Container } from "@/components/Container"
+import { Content } from "@/components/Content"
+import { Header } from "@/components/Header"
+import 'tailwindcss/tailwind.css';
+import { Card } from "@/components/Card";
 
 const Custom404 = () => {
     return(
-        <div>
-            <Breadcrumbs/>
-            <h1>Ops, algo deu errado! Esta página não existe!</h1>
-        </div>
+        <Container>
+          <Header />
+          <Content>
+            <Card
+            imageURL="https://flyingcdn-f1b39866.b-cdn.net/wp-content/uploads/2022/04/404.gif"
+            nomeProduto="404 Não Encontrado"
+            linkCompra="/"
+            data="01/01/01"
+            hora="00:00"
+            />
+        </Content>
+        </Container>
     )
 }
 
 export default Custom404;
+
+
+
+
