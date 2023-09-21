245013c5d5cca89e7700dbd3616e315195a73e02
KillerDogs <kenybustamante2@hotmail.com>
2023-09-21 15:53:27 -0300

    create product page

---
+++ pages/promo/[ItemId].js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+import { Container } from "@/components/Container";
+
+
+export function promoPage() {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
