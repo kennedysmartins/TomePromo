a6f55b3073616ce8a0fd7b6587e68e3617eb8c28
KillerDogs <kenybustamante2@hotmail.com>
2023-09-21 20:16:07 -0300

    changes Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,15 @@ import React from 'react';
 
 export function Header() {
     return (
-        <header className="w-full p-3 bg-gray-50 border-b-slate-100 border fixed z-10">
-            <div className='flex gap-3 items-center max-w-5xl mx-auto'>
-                <img className='rounded-full w-9 justify-center ' src='/logo-tomepromo.jpeg'></img>
-                <h1 className='text-xl font-semibold text-black' >Tome Promo</h1>
+        <header className="flex justify-between w-full px-96 py-4 bg-gray-50 border-b-slate-100 border fixed z-10">
+            <div className='max-w-5xl'>
+                <a href='/' className='flex gap-3 items-center'>
+                    <img className='rounded-full w-9 justify-center ' src='/logo-tomepromo.jpeg'></img>
+                    <h1 className='text-xl font-semibold text-black' >Tome Promo</h1>
+                </a>
             </div>
 
-            <div>
+            <div className='items-center pt-1'>
                 <Link href={"/admin"}>Admin Panel</Link>
             </div>
 
