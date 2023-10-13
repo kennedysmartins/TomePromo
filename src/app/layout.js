0e295efc00cca63270059172a1d8018003ff5caf
kennedysmartins <kennedysmartins@hotmail.com>
2023-10-12 21:38:15 -0300

    Change Name

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,13 +4,14 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Rocketseat - Tome Promo',
+  title: 'Infnet - Tome Promo',
   description: 'Quer promo? Tome!',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="manifest" href="manifest.json"></link>
       <body className={inter.className}>{children}</body>
     </html>
   )
