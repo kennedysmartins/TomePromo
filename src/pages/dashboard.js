b84b3cc5ac602c0c8d0a07ac582eae22fffda6c8
kennedysmartins <kennedysmartins@hotmail.com>
2023-10-12 21:38:44 -0300

    Dashboard changes

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,53 +1,37 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Container } from "@/components/Container";
 import { Content } from "@/components/Content";
 import { Header } from "@/components/Header";
 import { Sidebar } from "@/components/Sidebar";
 import { Bottom } from "@/components/Bottom";
 
-import 'tailwindcss/tailwind.css';
+import "tailwindcss/tailwind.css";
 import { Box } from "@/components/Box";
-
 
 const Dashboard = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-  const handleMenuToggle  = () => {
-    setIsDrawerOpen(!isDrawerOpen)
-  }
+  const handleMenuToggle = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
 
   return (
     <Container bgActive={false}>
       <Header onMenuToggle={handleMenuToggle} />
       <div className="flex">
-      <Sidebar className={`hidden md:flex flex-col`} isOpen={isDrawerOpen} onClose={handleMenuToggle} />
-        <Content>
-            <div className=" flex flex-col">
-              <br></br>
-              <div className="mb-4">
-                <h1 className="mt-2 text-4xl p-4  ">Dashboard</h1>
-                <h1 className=" text-lg px-4  ">Principal</h1>
-              </div>
-              <div className="flex w-full">
-              <Box>
-                <h2>Quantidade de Produtos</h2>
-                <div>
-                  <picture></picture>
-                  <h3 className="text-8xl">85</h3>
-                </div>
-              </Box>
-              <Box>
-                <h2>Clicks/24 horas</h2>
-                <div>
-                  <picture></picture>
-                  <h3 className="text-8xl">182</h3>
-                </div>
-              </Box>
-              </div>
-            </div>
-            <Bottom className={`md:hidden`} />
-        </Content>
+        <Sidebar
+          className={`hidden md:flex flex-col`}
+          isOpen={isDrawerOpen}
+          onClose={handleMenuToggle}
+        />
+        <div className="mt-16">
+          <h1 className="mt-2 text-4xl p-4  ">Dashboard</h1>
+          <Box>
+            <h3>Criar oferta</h3>
+          </Box>
+        </div>
       </div>
+      <Bottom className={`md:hidden`} />
     </Container>
   );
 };
