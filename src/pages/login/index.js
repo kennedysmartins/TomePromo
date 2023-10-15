a5f9a3384ce3a03841ca467edeaf12fb76c9b607
kennedysmartins <kennedysmartins@hotmail.com>
2023-10-15 20:55:48 -0300

    change main page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -7,10 +7,13 @@ import { Content } from "@/components/Content";
 import { Header } from "@/components/Header";
 import { Sidebar } from "@/components/Sidebar";
 import { Bottom } from "@/components/Bottom";
+import CompleteProfile from '@/components/CompleteProfile'
 import "tailwindcss/tailwind.css";
 
 const LoginPage = () => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    console.log("process.env em string:", JSON.stringify(process.env));
+
 
   const handleMenuToggle  = () => {
     setIsDrawerOpen(!isDrawerOpen)
@@ -33,10 +36,23 @@ const LoginPage = () => {
             <div className=" flex flex-col">
               <br></br>
               <div>
-                <h1 className="mt-2 text-4xl p-4  ">Categorias</h1>
+                <h1 className="mt-2 text-4xl p-4  ">Você está logado</h1>
+
                 <h1 className=" text-lg px-4  ">
-                  Crie e gerencie as categorias
+                Logado como: {session.user.name}, e-mail: {session.user.email}
                 </h1>
+                
+              <div>
+                <CompleteProfile user={session.user}/>
+            </div>
+            <button
+                className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 ml-4 mt-2 px-4 rounded-full'
+                onClick={() => {
+                  signOut();
+                }}
+              >
+                Sign out
+              </button>
               </div>
             </div>
             <Bottom className={`md:hidden`} />
@@ -61,7 +77,7 @@ const LoginPage = () => {
               <h1 className="mt-2 text-4xl p-4  ">Você não está logado</h1>
               <h1 className=" text-lg px-4  ">Logue com o Google!</h1>
               <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                className="bg-blue-500 ml-4 mt-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
                 onClick={() => signIn()}
               >
                 Sign in
