06b6835c7f65d5d916271b6c460bdbe9b0789c7f
kennedysmartins <kennedysmartins@hotmail.com>
2023-10-15 22:36:35 -0300

    Login withou redirect

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -77,11 +77,11 @@ const LoginPage = () => {
               <h1 className="mt-2 text-4xl p-4  ">Você não está logado</h1>
               <h1 className=" text-lg px-4  ">Logue com o Google!</h1>
               <button
-                className="bg-blue-500 ml-4 mt-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-                onClick={() => signIn()}
-              >
-                Sign in
-              </button>
+  className="bg-blue-500 ml-4 mt-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+  onClick={() => signIn('google', { callbackUrl: '/', redirect: false })}
+>
+  Sign in
+</button>
             </div>
           </div>
           <Bottom className={`md:hidden`} />
