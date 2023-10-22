bf927d4dfc0100bc96200c209c1b03ede6d4bd3a
kennedysmartins <kennedysmartins@hotmail.com>
2023-10-22 19:36:26 -0300

    new component Box using styled-components

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,11 +1,13 @@
+import { Container } from './styles'
+
 import React, {useContext} from 'react';
 import { ThemeContext } from '@/contexts/ThemeContext'
 
 export function Box({children}) {
     const { theme } = useContext(ThemeContext);
     return (
-        <div className={`${theme === 'dark' ? 'bg-gray-800 text-white': 'bg-white'} w-full  rounded p-4 m-4`}>
+        <Container theme={theme}>
                 {children}
-        </div>
+        </Container>
     )
 }
---
+++ src/components/Box/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  border-radius: 6px;
+  background-color: ${({ theme }) => (theme === 'dark' ? '#2d3748' : '#fff')};
+  color: ${({ theme }) => (theme === 'dark' ? '#fff' : '#000')};
+  width: 100%;
+  padding: 16px;
+  margin: 16px;
+`;
