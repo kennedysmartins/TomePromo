dbf523dcd03181ac3fed8c97ab8ad9901db9954e
KillerDogs <kenybustamante2@hotmail.com>
2023-10-25 23:08:10 -0300

    edited component Input

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { ThemeContext } from '@/contexts/ThemeContext' 
+
 
 export function Input({icon: Icon, ...rest}) {
+  const { theme } = useContext(ThemeContext);
+
     return (
         <div>
 
             {Icon && <Icon size={20}/>}
             <input 
-            className='w-full'
+            className={`${theme === 'dark' ? 'bg-gray-800 text-white shadow-slate-700/10': 'bg-zinc-50 text-zinc-900 shadow-slate-300/30'} rounded p-2 w-full `}
             {...rest} />
         </div>
 
