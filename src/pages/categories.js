29676d36b65e6eddf33c8ddb758064a58b89211c
KillerDogs <kenybustamante2@hotmail.com>
2023-10-25 23:08:25 -0300

    get categories from API

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -7,6 +7,8 @@ import { Header } from "@/components/Header";
 import { Sidebar } from "@/components/Sidebar";
 import { Bottom } from "@/components/Bottom";
 import { DrawerContext } from "@/contexts/DrawerContext";
+import { getCategories } from "@/utils/api";
+
 
 import "tailwindcss/tailwind.css";
 
@@ -16,6 +18,18 @@ const Categories = () => {
   const [shouldRender, setShouldRender] = useState(false);
   const { drawer, toggleDrawer } = useContext(DrawerContext);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const [categories, setCategories] = useState(Array(3).fill({})) 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const dataCategories = await getCategories(); 
+      const updatedCategories = [...dataCategories.slice(0, 20)];
+      setCategories(updatedCategories);
+    };
+
+    fetchData();
+  }, [])
 
   useEffect(() => {
     const checkSessionAndRedirect = async () => {
@@ -54,6 +68,18 @@ const Categories = () => {
         <Content className="m-8">
           <h1 className="mt-2 text-4xl p-4 ">Categorias</h1>
           <h2 className="text-lg px-4  ">Crie e gerencie as categorias</h2>
+          <div className="p-4">
+          <ul>
+                  {categories.map((categorie) => (
+                      <li key={categorie.id}>
+                        <div className="">
+                          
+                          <p className="border rounded p-2 my-2 shadow-md">{categorie.name}</p>
+                        </div>
+                      </li>
+                    ))}
+                </ul>
+          </div>
         </Content>
       </div>
       <Bottom className={`md:hidden`} />
