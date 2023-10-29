d03fcb2d48840dfbcc22d2a7024eaf9ed00d7c8f
KillerDogs <kenybustamante2@hotmail.com>
2023-10-29 00:02:27 -0300

    changes className Input

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import { ThemeContext } from '@/contexts/ThemeContext' 
 
 
-export function Input({icon: Icon, ...rest}) {
+export function Input({icon: Icon, className, ...rest}) {
   const { theme } = useContext(ThemeContext);
 
     return (
@@ -10,7 +10,7 @@ export function Input({icon: Icon, ...rest}) {
 
             {Icon && <Icon size={20}/>}
             <input 
-            className={`${theme === 'dark' ? 'bg-gray-800 text-white shadow-slate-700/10': 'bg-zinc-50 text-zinc-900 shadow-slate-300/30'} rounded p-2 w-full `}
+            className={`${theme === 'dark' ? 'bg-gray-800 text-white shadow-slate-700/10': 'bg-zinc-50 text-zinc-900 shadow-slate-300/30'} rounded p-2 w-full ${className}`}
             {...rest} />
         </div>
 
