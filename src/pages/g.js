2955cd67c20832b572ed12073757069f2d598df7
KillerDogs <kenybustamante2@hotmail.com>
2023-11-06 01:34:32 -0300

    update page Head

--- src/pages/g.js
+++ src/pages/g.js
@@ -15,14 +15,14 @@ export default function Grupo() {
   return <Container>
     <Head>
         <title>Grupo de Promoções - TomePromo</title>
-        <meta property="og:title" content="Grupo de Promoções" />
-        <meta property="og:image" content="https://www.tomepromo.com.br/logo-tomepromo1.jpeg" />
+        <meta property="og:title" content="Black Friday - TomePromo" />
+        <meta property="og:image" content="https://www.tomepromo.com.br/blackfriday-tomepromo.png" />
         <meta property="og:url" content={`https://tomepromo.com.br/g/`} />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="TomePromo" />
         <meta property="og:locale" content="pt_BR" />
         <meta property="og:locale:alternate" content="en_US" />
-        <meta name="description" content="Quer promoção? então Tome! - TomePromo" /> 
+        <meta name="description" content="Aproveite as ofertas do esquenta Black Friday - TomePromo" /> 
       </Head>
     Redirecionando...
     </Container>;
